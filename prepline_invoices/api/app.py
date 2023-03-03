4ef36f1c93006c17bde49cf01102781d595af84f
Antonio Jimeno Yepes <antonio@unstructured.io>
2023-03-03 21:46:09 +1100

    API regeneration

--- prepline_invoices/api/app.py
+++ prepline_invoices/api/app.py
@@ -14,7 +14,11 @@ from .invoices import router as invoices_router
 
 
 limiter = Limiter(key_func=get_remote_address)
-app = FastAPI()
+app = FastAPI(
+    title="Unstructured Pipeline API",
+    description="""""",
+    version="1.0.0",
+)
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
